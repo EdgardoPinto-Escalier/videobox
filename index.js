5d7af771f97c28aaecf2ab17403b968fde23e4af
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-08-27 13:16:13 +0200

    Added persistence to the Genres API

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const Joi = require('joi');
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 const express = require('express');
 const app = express();
+
+mongoose.connect('mongodb://localhost/videobox', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Unable to connect to MongoDB'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
